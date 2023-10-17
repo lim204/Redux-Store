fd48fa35139bcf16711245c3dc5b13df29a0c948
Laura M <laurita_im@hotmail.com>
2023-10-16 21:32:11 -0700

    first inputs in resolvers and typeDefs

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,6 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User, Product, Category, Order } = require('../models');
+const { signToken } = require('../utils/auth');
+// const stripe = require('stripe')
+
+module.exports = resolvers;
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,45 @@
+const {gql} = require ('apollo-server-express');
+
+const typeDefs = gql`
+    type Category {
+        _id: ID
+        name: String
+    }
+
+    type Product {
+    _id: ID
+    name: String
+    description: String
+    image: String
+    quantity: Int
+    price: Float
+    category: category
+    }
+
+    type Order{
+    _id: ID
+    purchaseDate: String
+    products: [Product]
+    }
+
+    type User{
+    _id: ID
+    firstName: String
+    lastName: String
+    email: String
+    Orders: [Order]
+    }
+
+    type Checkout:{
+    session: ID
+    }
+
+    type Auth{
+    token: ID
+    user: User
+    }
+
+
+
+`;
+module.exports = typeDefs;
