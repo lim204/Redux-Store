5f635838ec16cc62c9b9838dd584a97919ad5d49
Laura M <laurita_im@hotmail.com>
2023-10-18 20:58:42 -0700

    added mutations

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -48,5 +48,12 @@ const typeDefs = gql`
     checkout(products: [ID]!):Checkout
     }
 
+    type Mutation {
+    addUser (firstName: String!, lastName:String!, email: String!, password:String!): Auth
+    addOrder (products: [ID]!): Order
+    updateUser(firstName: String!, lastName:String!, email: String!, password:String!): User
+    updateProduct(_id: ID!, quantity: Int!): Product
+    login(email: String!, password: String!): Auth
+    }
 `;
 module.exports = typeDefs;
