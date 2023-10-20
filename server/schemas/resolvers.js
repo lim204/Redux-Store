404782c75d7bae2f6586e5a9dcbdb19fe5370680
Laura M <laurita_im@hotmail.com>
2023-10-19 21:03:44 -0700

    first inputs in resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,4 +3,25 @@ const { User, Product, Category, Order } = require('../models');
 const { signToken } = require('../utils/auth');
 // const stripe = require('stripe')
 
+const resolvers = {
+    Query: {
+        categories: async () => {
+            return await Category.find();
+        },
+        products: async (parent,{categories, name}) => {
+            const params = {};
+            if (category){
+                params.category = category;
+            }
+
+            if (name){
+                params.name = {
+                    $regex: name
+                };
+            }
+          return await Product.find(params).populate('category');
+        },
+    }
+}
+
 module.exports = resolvers;
