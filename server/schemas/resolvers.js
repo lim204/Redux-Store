a8849696727fc586b4759ca1422afa327d507999
Laura M <laurita_im@hotmail.com>
2023-10-21 20:58:37 -0700

    Adding order and checkout

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,6 +1,7 @@
 const { AuthenticationError } = require('apollo-server-express');
 const { User, Product, Category, Order } = require('../models');
 const { signToken } = require('../utils/auth');
+const { argsToArgsConfig } = require('graphql/type/definition');
 // const stripe = require('stripe')
 
 const resolvers = {
@@ -37,7 +38,44 @@ const resolvers = {
             }
             throw new AuthenticationError('Not logged in');
         },
-        
+        order: async(parent,{_id}, context)=>{
+            if(context.user) {
+                const user = await User.findById(context.user._id).populate
+                ({
+                    path:'orders.products',
+                    populate:'category'
+                });
+                return user.orders.id(_id);
+            }
+            throw new AuthenticationError('Not logged in');
+        },
+        checkout: async (parent,arg,context)=> {
+            const url = new URL(context.headers.referer).origin;
+            const order = new Order({producr:argsToArgsConfig.products});
+            const line_items =[];
+
+            const {products} = await order.populate('products');
+
+            for (let i = 0; i<products.length;i++){
+                const product = await stripIgnoredCharacters.product.create({
+                    name:products[i].name,
+                    description: products[i].description,
+                    images: [`${url}/images/${products[i].image}`]
+                });
+                const price = await stripe.prices.create({
+                    product: product.id,
+                    unit_amount: products[i].price * 100,
+                    currency: 'usd',
+                  });
+          
+                  line_items.push({
+                    price: price.id,
+                    quantity: 1
+                  });
+            }
+            
+        }
+
     }
 }
 
